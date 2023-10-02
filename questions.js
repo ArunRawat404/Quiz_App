e291a4fbabe69f7378460f193e5afa49e3f28938
arunrawat404 <arunrawatar404@gmail.com>
2023-10-02 20:00:15 +0530

    Quiz App

---
+++ questions.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question: 'Which of these is the correct way in which we can call the JavaScript code?',
+        options: ['Function/Method', 'Triggering Event', 'Preprocessor', 'RMI'],
+        answer: 'Function/Method',
+    },
+    {
+        question: 'JSON stands for _______',
+        options: ['JavaScript Object Notation', 'Java Object Notation', 'JavaScript Object Normalization', 'JavaScript Object-Oriented Notation'],
+        answer: 'JavaScript Object Notation',
+    },
+    {
+        question: 'Which country won the FIFA World Cup in 2018?',
+        options: ['Brazil', 'Germany', 'France', 'Argentina'],
+        answer: 'France',
+    },
+    {
+        question: 'What is the tallest mountain in the world?',
+        options: ['Mount Everest', 'K2', 'Kangchenjunga', 'Makalu'],
+        answer: 'Mount Everest',
+    },
+    {
+        question: 'Which is the largest ocean on Earth?',
+        options: [
+            'Pacific Ocean',
+            'Indian Ocean',
+            'Atlantic Ocean',
+            'Arctic Ocean',
+        ],
+        answer: 'Pacific Ocean',
+    },
+];
+
+export default quizData;
